a825f35ac0c4f29de9a6bef4ded0a01d2376947e
= <=>
2023-04-24 21:34:28 -0400

    created project + server

---
+++ app.js
@@ -0,0 +1,38 @@
+// set up the server
+const express = require("express");
+const app = express()
+const port = 3000;
+const logger = require("morgan");
+
+// define middleware that logs all incoming requests
+app.use(logger("dev"));
+
+//use public as the folder for static files
+app.use(express.static(__dirname + '/public'));
+
+// define a route for the default home page
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/Lab1-pg1.html");
+});
+
+// start the server
+app.listen(port, () => {
+    console.log(`app listening at localhost:${port}`);
+});
+
+// define a route for the saved recipe page 
+app.get("/savedrecipe", (req, res) => {
+    res.send(__dirname + "/views/Lab1-pg2.html");
+})
+
+// define a route for the saved recipe detail page 
+app.get("/savedrecipe/details", (req, res) => {
+    res.send(__dirname + "/views/Lab1-pg3.html");
+})
+
+// define a route for the recipe page
+app.get( "/recipe", ( req, res ) => {
+    res.sendFile( __dirname + "/views/Lab1-pg4.html" );
+} );
+
+
